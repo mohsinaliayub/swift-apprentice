36cdd1c487bb26387431cf792da6951bfb1b0179
Mohsin Ali Ayub <mosin.ayub@outlook.com>
2025-10-06 20:19:58 +0200

    add challenge 1 starter playground

---
+++ 10-regex/Challenge1.playground/Contents.swift
@@ -0,0 +1,39 @@
+
+
+let capsRegex = /\b[a-z0-9]*[A-Z]{2,}[a-z0-9]*\b/
+
+let testingString1 = "abcdef ABCDEF 12345 abc123 ABC 123 123ABC 123abc abcABC"
+
+print("Test 1:")
+
+for match in testingString1.matches(of: capsRegex) {
+    print(String(match.output))
+}
+
+print("----------------")
+print("Test 2:")
+
+let testingString2 = "abcdeABCDE1234 1234ABCDEabcde abcde1234"
+
+for match in testingString2.matches(of: capsRegex) {
+    print(String(match.output))
+}
+
+print("----------------")
+print("Test 3:")
+
+let testingString3 = "a1b2ABCDEc3d4"
+
+for match in testingString3.matches(of: capsRegex) {
+    print(String(match.output))
+}
+
+print("----------------")
+print("Test 4: No Matches")
+
+let testingString4 = "abcA12a3 abc123"
+
+for match in testingString4.matches(of: capsRegex) {
+    print(String(match.output))
+}
+print("----------------")
