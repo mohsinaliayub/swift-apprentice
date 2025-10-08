976256c2e326b223d1d8f125b5d45b989af0a9fe
Mohsin Ali Ayub <mosin.ayub@outlook.com>
2025-10-08 23:19:51 +0200

    solve challenges

--- 13-methods/13-methods-challenges.playground/Contents.swift
+++ 13-methods/13-methods-challenges.playground/Contents.swift
@@ -1,34 +1,3 @@
-/// Copyright (c) 2025 Kodeco LLC
-///
-/// Permission is hereby granted, free of charge, to any person obtaining a copy
-/// of this software and associated documentation files (the "Software"), to deal
-/// in the Software without restriction, including without limitation the rights
-/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-/// copies of the Software, and to permit persons to whom the Software is
-/// furnished to do so, subject to the following conditions:
-///
-/// The above copyright notice and this permission notice shall be included in
-/// all copies or substantial portions of the Software.
-///
-/// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
-/// distribute, sublicense, create a derivative work, and/or sell copies of the
-/// Software in any work that is designed, intended, or marketed for pedagogical or
-/// instructional purposes related to programming, coding, application development,
-/// or information technology.  Permission for such use, copying, modification,
-/// merger, publication, distribution, sublicensing, creation of derivative works,
-/// or sale is expressly withheld.
-///
-/// This project and source code may use libraries or frameworks that are
-/// released under various Open-Source licenses. Use of those libraries and
-/// frameworks are governed by their own individual licenses.
-///
-/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-/// THE SOFTWARE.
 
 /*:
  
@@ -53,22 +22,22 @@
  Hint: Add a setter to `area`.
 */
 struct Circle {
-  
-  var radius = 0.0
-
-  var area: Double {
-    get {
-      .pi * radius * radius
+    
+    var radius = 0.0
+    
+    var area: Double {
+        get {
+            .pi * radius * radius
+        }
+        set {
+            radius = (newValue / .pi).squareRoot()
+        }
     }
-    set {
-      radius = (newValue / .pi).squareRoot()
+    
+    mutating func grow(byFactor factor: Double) {
+        area *= factor
     }
-  }
-
-  mutating func grow(byFactor factor: Double) {
-    area *= factor
-  }
-  
+    
 }
 
 var circle = Circle(radius: 5)
@@ -110,39 +79,39 @@ let months = ["January", "February", "March",
               "October", "November", "December"]
 
 struct SimpleDate {
-  var month: String
-  var day: Int
-  
-  var totalDaysInCurrentMonth: Int {
-    switch month {
-    case "January", "March", "May", "July", "August", "October", "December":
-      return 31
-    case "April", "June", "September", "November":
-      return 30
-    case "February": // Note: leap years aren't taken into account here.
-      return 28
-    default:
-      return 0
+    var month: String
+    var day: Int
+    
+    var totalDaysInCurrentMonth: Int {
+        switch month {
+        case "January", "March", "May", "July", "August", "October", "December":
+            return 31
+        case "April", "June", "September", "November":
+            return 30
+        case "February": // Note: leap years aren't taken into account here.
+            return 28
+        default:
+            return 0
+        }
     }
-  }
-  
-  mutating func advance() {
-    // Check for the end of the month
-    if day == totalDaysInCurrentMonth {
-      // Check for the end of the year
-      if month == "December" {
-        month = "January"
-      } else {
-        // Increment the month
-        month = months[months.firstIndex(of: month)!.advanced(by: 1)]
-      }
-      // Start over at the first day of the month
-      day = 1
-    } else {
-      // It is not the end of the month, just increment the day
-      day += 1
+    
+    mutating func advance() {
+        // Check for the end of the month
+        if day == totalDaysInCurrentMonth {
+            // Check for the end of the year
+            if month == "December" {
+                month = "January"
+            } else {
+                // Increment the month
+                month = months[months.firstIndex(of: month)!.advanced(by: 1)]
+            }
+            // Start over at the first day of the month
+            day = 1
+        } else {
+            // It is not the end of the month, just increment the day
+            day += 1
+        }
     }
-  }
 }
 
 var date = SimpleDate(month: "December", day: 31)
@@ -156,12 +125,12 @@ date.day // 1
 struct Math {}
 
 extension Math {
-  static func isEven(_ value: Int) -> Bool {
-    value % 2 == 0
-  }
-  static func isOdd(_ value: Int) -> Bool {
-    (value + 1) % 2 == 0
-  }
+    static func isEven(_ value: Int) -> Bool {
+        value % 2 == 0
+    }
+    static func isOdd(_ value: Int) -> Bool {
+        (value + 1) % 2 == 0
+    }
 }
 
 Math.isOdd(30) // false
@@ -175,12 +144,12 @@ Math.isEven(21) // false
  > Generally, you want to be careful about what functionality you add to standard library types as it can cause confusion for readers.
  */
 extension Int {
-  var isEven: Bool {
-    self % 2 == 0
-  }
-  var isOdd: Bool {
-    (self + 1) % 2 == 0
-  }
+    var isEven: Bool {
+        self % 2 == 0
+    }
+    var isOdd: Bool {
+        (self + 1) % 2 == 0
+    }
 }
 
 2.isEven // true
@@ -192,24 +161,24 @@ extension Int {
 Add the method `primeFactors()` to `Int`. Since this is an expensive operation, this is best left as an actual method.
 */
 extension Int {
-  func primeFactors() -> [Int] {
-    var remainingValue = self
-    var testFactor = 2
-    var primes: [Int] = []
-    while testFactor * testFactor <= remainingValue {
-      if remainingValue % testFactor == 0 {
-        primes.append(testFactor)
-        remainingValue /= testFactor
-      }
-      else {
-        testFactor += 1
-      }
+    func primeFactors() -> [Int] {
+        var remainingValue = self
+        var testFactor = 2
+        var primes: [Int] = []
+        while testFactor * testFactor <= remainingValue {
+            if remainingValue % testFactor == 0 {
+                primes.append(testFactor)
+                remainingValue /= testFactor
+            }
+            else {
+                testFactor += 1
+            }
+        }
+        if remainingValue > 1 {
+            primes.append(remainingValue)
+        }
+        return primes
     }
-    if remainingValue > 1 {
-      primes.append(remainingValue)
-    }
-    return primes
-  }
 }
 
 81.primeFactors() // [3, 3, 3, 3]
